1d8c46e882aec7960ceb307e0666f84e368747ff
= <riyanartz4@gmail.com>
2022-09-10 12:49:59 +0700

    commit kedua

---
+++ operator2.php
@@ -0,0 +1,14 @@
+<?php 
+$x=10;
+$y=6; 
+echo ($x + $y) . "<br>"; 
+echo ($x - $y) . "<br>"; 
+echo ($x * $y) . "<br>"; 
+echo ($x / $y) . "<br>"; 
+echo ($x % $y) . "<br>"; 
+$a = "Hello"; 
+$b = $a . " world!"; 
+echo $b . "<br>";  
+echo $a. " -- " . $b. " ini string operator " . "<br>"; 
+?>
+
--- variable.php
+++ variable.php
@@ -1,6 +1,21 @@
-<?php
-$nim="E12345678";
-$nama="Budi"; 
-$prodi="Teknik Informatika"; 
-echo "Selamat Praktikum \n $nim <br> $nama <br> $prodi <br>";
-?>
+<?php 
+$x = 5985;
+ var_dump($x);
+  echo "<br>";
+   $x = -345;// negative number 
+    var_dump($x);
+     echo "<br>"; 
+     $x = 0x8C; // hexadecimal number 
+     var_dump($x); 
+     echo "<br>"; 
+     $x = 047; // octal number 
+     var_dump($x); 
+     $x = 10.365; var_dump($x);
+      echo "<br>"; $x = 2.4e3;
+       var_dump($x); 
+       echo "<br>"; 
+       $x = 8E-5; 
+       var_dump($x);
+        echo strlen("Hello world!"); 
+        echo strpos("Hello world!","world"); 
+        ?>
